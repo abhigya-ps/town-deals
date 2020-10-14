8e31d46a4a84e8756266280e2f517b069abd71f7
Pote Shrestha <poteshresthaa@wit.edu>
2020-10-13 23:36:47 -0400

    static url

--- towndeals/settings.py
+++ towndeals/settings.py
@@ -161,13 +161,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # AWS_ACCESS_KEY_ID = os.environ.get('TD_AWS_ACCESS_KEY_ID')
 # AWS_SECRET_ACCESS_KEY = os.environ.get('TD_AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'towndeals-bucket'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
-# ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+STATIC_URL = 'http://towndeals-bucket.s3.amazonaws.com/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 
 
