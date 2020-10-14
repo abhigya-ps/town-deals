155cab0f081f01b4d981ecfad2ab6598a0f3d0ae
Pote Shrestha <poteshresthaa@wit.edu>
2020-10-14 10:59:59 -0400

    env variables

--- towndeals/settings.py
+++ towndeals/settings.py
@@ -23,10 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# SECRET_KEY = os.environ.get('TD_SECRET_KEY')
-
 SECRET_KEY = os.environ.get('TD_SECRET_KEY')
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
